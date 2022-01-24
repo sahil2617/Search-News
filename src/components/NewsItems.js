7ad435ef1b7b4df7d33ebb75e1747f09c7cc6252
sahil <sahilshaikh2286@gmail.com>
2022-01-24 17:01:29 +0530

    content loading animation

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -4,12 +4,12 @@ export default class NewsItems extends Component {
   render() {
     return (
         <>
-            <div className="card my-3" style={{width: '18rem'}}>
+            <div className="card my-3">
                 <img src={this.props.imgUrl} className="card-img-top" alt="..."/>
                 <div className="card-body">
-                    <h5 className="card-title">{this.props.title.slice(0,30)}...</h5>
-                    <p className="card-text">{this.props.description.slice(0,100)}...</p>
-                    <a href={this.props.url}  className="btn btn-sm btn-primary">Read more</a>
+                    <h5 className="card-title">{this.props.title}</h5>
+                    <p className="card-text">{this.props.description}</p>
+                    <a href={this.props.url} target="_blank" className="btn btn-sm btn-primary">Read more</a>
                 </div>
             </div>
         </>
